0cb780437fabc0bc9741df8b948b8183f0ad021b
zrendy <zrendy@gmail.com>
2022-10-18 19:54:05 -0400

    ratings and review, server

--- atelier/src/App.js
+++ atelier/src/App.js
@@ -1,17 +1,16 @@
 import Navbar from './components/Navbar.js'
 import Overview from './components/overview/Overview.js'
-import './App.css';
+import RatingsAndReviews from './components/ratings-reviews/RatingsAndReviews.js'
+
 
 function App() {
-
-
 
   return (
 
     <div className="App">
       <Navbar/>
       <Overview/>
-
+      <RatingsAndReviews/>
       <section></section>
       <aside></aside>
       <section></section>
