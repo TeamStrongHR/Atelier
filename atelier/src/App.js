945b584bd1c2decdd1a6520d9bb99ab14efb28c6
Lu Dong <odong0813@gmail.com>
2022-10-18 20:31:57 -0400

    Resolving merge conflicts

--- atelier/src/App.js
+++ atelier/src/App.js
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
 import Navbar from './components/Navbar.js';
 import Overview from './components/overview/Overview.js';
 import RelatedAndComparison from './components/related-items/RelatedAndComparison.js';
-import './App.css';
-=======
-import Navbar from './components/Navbar.js'
-import Overview from './components/overview/Overview.js'
 import RatingsAndReviews from './components/ratings-reviews/RatingsAndReviews.js'
 
->>>>>>> 5463395e3aabd7363d19eb564481c606aad19e87
 
 function App() {
 
@@ -17,11 +11,8 @@ function App() {
     <div className="App">
       <Navbar/>
       <Overview/>
-<<<<<<< HEAD
       <RelatedAndComparison />
-=======
       <RatingsAndReviews/>
->>>>>>> 5463395e3aabd7363d19eb564481c606aad19e87
       <section></section>
       <aside></aside>
       <section></section>
