a7f57644ceb1c96168e0c476a136aca24d8f6fed
Young Min Ko <dudals3394@gmail.com>
2022-10-20 16:08:46 -0400

    progress demo; still need to refactor carousel

--- atelier/src/components/overview/carousel/ImageGallery.js
+++ atelier/src/components/overview/carousel/ImageGallery.js
@@ -1,14 +1,13 @@
 // import axios from 'axios';
 import {useState, useEffect} from 'react';
 import MainImage from './MainImage.js'
-import Thumbnail from './Thumbnail.js'
 import image1 from '../../../images/image1.jpeg'
 import image2 from '../../../images/image2.jpeg'
 import image3 from '../../../images/image3.jpeg'
 import image4 from '../../../images/image4.jpeg'
 function ImageGallery() {
-  let imageIndex =0;
-  let thumbnailIndex= 0;
+  const [index, setImageIndex] = useState([0,0]);
+
   let imageArr = [image1, image2, image3, image4, image2, image3];
 
   // helper function for carousel
@@ -16,23 +15,23 @@ function ImageGallery() {
     let imageArr = document.getElementsByClassName('main-image');
     let thumbArr = document.getElementsByClassName('thumbnail');
     // console.log(imageArr);
-    if (num > imageArr.length -1) {imageIndex = 0;}
-    else if (num < 0) {imageIndex = imageArr.length-1}
-    else {imageIndex = num};
-    for (let i = 0; i < imageArr.length; i++) {
-      if (i === imageIndex) {
-        imageArr[i].style.display ="flex";
-      } else {
-        imageArr[i].style.display="none";
-      }
-    }
-    for (let i = 0; i < thumbArr.length; i++) {
-      if (i === imageIndex) {
-        thumbArr[i].style.border = "thick solid";
-      } else {
-        thumbArr[i].style.border = "none";
-      }
-    }
+    // if (num > imageArr.length -1) {imageIndex = 0;}
+    // else if (num < 0) {imageIndex = imageArr.length-1}
+    // else {imageIndex = num};
+    // for (let i = 0; i < imageArr.length; i++) {
+    //   if (i === imageIndex) {
+    //     imageArr[i].style.display ="flex";
+    //   } else {
+    //     imageArr[i].style.display="none";
+    //   }
+    // }
+    // for (let i = 0; i < thumbArr.length; i++) {
+    //   if (i === index) {
+    //     thumbArr[i].style.border = "thick solid";
+    //   } else {
+    //     thumbArr[i].style.border = "none";
+    //   }
+    // }
   };
 
   const showThumbnails = (num) => {
@@ -43,7 +42,7 @@ function ImageGallery() {
     if (num < 0) {
       num = 0;
     }
-    thumbnailIndex = num;
+    // thumbnailIndex = num;
     for (let i = 0; i < thumbArr.length; i++) {
       if (i >= num && i <num+4) {
         thumbArr[i].style.display ="flex";
@@ -55,11 +54,11 @@ function ImageGallery() {
   };
 
   const moveThumbnail = (num)=>{
-    showThumbnails(thumbnailIndex+=num)
+
   };
 
   const moveImage = (num)=> {
-    showImage(imageIndex+=num);
+
   };
 
   useEffect(()=>{
