5afb15b90df2d38168b54b22603c7a83f1d9896b
Lu Dong <odong0813@gmail.com>
2022-10-20 17:28:10 -0400

    Preparing for api and server request AND minor scss

--- atelier/src/components/related-items/RelatedAndComparison.js
+++ atelier/src/components/related-items/RelatedAndComparison.js
@@ -2,11 +2,22 @@ import {useState, useEffect} from 'react';
 import RelatedList from './RelatedList.js';
 import YourOutfit from './YourOutfit.js';
 import {testProduct, testProductArr, testProductInfo, testProductStyles} from './testData.js';
-
+import axios from 'axios';
 
 export default function RelatedAndComparison (props) {
   const [currProduct, setCurrProduct] = useState(testProduct);
 
+  useEffect(()=> {
+    var options = {
+      url: `http://localhost:${process.env.PORT}/api/related`,
+      method: "get",
+    }
+    axios(options)
+    .then(result => {
+      console.log(result);
+    })
+    .catch(err => console.log(err));
+  })
   return (
     <div className='related-comparison'>
       <RelatedList currentProduct={currProduct} />
