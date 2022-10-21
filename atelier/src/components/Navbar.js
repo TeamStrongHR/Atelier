c40b1965dd9182b803ee3400897c153ad352b541
Young Min Ko <dudals3394@gmail.com>
2022-10-21 12:03:08 -0400

    navbar background color, banner justify-content center

--- atelier/src/components/Navbar.js
+++ atelier/src/components/Navbar.js
@@ -3,13 +3,13 @@
 export default function Navbar () {
   return (
   <div>
-  <header>
+  <header className="header">
     <nav className='navbar'>
       <span>LOGO</span>
       <span>SEARCH BAR</span>
     </nav>
   </header>
-  <h4>BANNER!! <a href="/">SALE SALE SALE!</a></h4>
+  <h4 className="banner">BANNER!! <a href="/">SALE SALE SALE!</a></h4>
   </div>
   )
 }
---
+++ atelier/src/components/overview/style-selector/StyleThumbnail.js
@@ -0,0 +1,8 @@
+
+const StyleThumbnail = ({thumbnail}) => {
+  return (
+    <div>
+      <img className="style-thumbnail" src="thumbnail"></img>
+    </div>
+  )
+}
