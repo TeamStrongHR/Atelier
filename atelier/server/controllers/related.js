f5e19260c0689c95515aba131d10c10f49f3902f
zrendy <zrendy@gmail.com>
2022-10-27 22:38:07 -0400

    final product

--- atelier/server/controllers/related.js
+++ atelier/server/controllers/related.js
@@ -50,11 +50,7 @@ const related = (req, res) => {
           result.slogan = data[1].data.slogan;
           result.description = data[1].data.description;
           let default_style = data[2].data.results;
-<<<<<<< HEAD
-          console.log('PRINT HERE ', data[2].headers['x-ratelimit-remaining']);
-=======
           console.log('PRINT HERE API REQUEST REMAINING',data[2].headers['x-ratelimit-remaining']);
->>>>>>> fae0c2c52352aec0f3a9dc499f4dfac4a22f2335
           for (let i = 0; i < default_style.length; i++) {
             if (default_style[i]['default?'] === true) {
               default_style.unshift(default_style.splice(i, 1)[0]);
@@ -88,13 +84,8 @@ const related = (req, res) => {
           res.json(result);
         })
         .catch((err) => {
-<<<<<<< HEAD
-          console.log('err', err.response);
-          res.status(424).end();
-=======
           console.log('err', err.response.status);
           res.status(err.response.status).end();
->>>>>>> fae0c2c52352aec0f3a9dc499f4dfac4a22f2335
         })
       break
 
